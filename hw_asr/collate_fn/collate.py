5dd0fff494f1ba6b5b2fbe19b58b43016d69a71f
ArseniyBolotin <arseniy.bolotin@gmail.com>
2021-10-15 02:12:33 +0300

    checkpoint

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,7 +1,17 @@
 import logging
 from typing import List
+import torch
+import torch.nn.functional as F
 
 logger = logging.getLogger(__name__)
+
+
+def last_dim_padding(dataset_items, key):
+    items_with_padding = []
+    max_size = max(list(item[key].size(-1) for item in dataset_items))
+    for item in dataset_items:
+        items_with_padding.append(F.pad(item[key], (0, max_size - item[key].size(-1))))
+    return torch.cat(tuple(items_with_padding), dim=0)
 
 
 def collate_fn(dataset_items: List[dict]):
@@ -10,5 +20,14 @@ def collate_fn(dataset_items: List[dict]):
     """
 
     result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+
+    for key in ('audio', 'spectrogram', 'text_encoded'):
+        result_batch[key] = last_dim_padding(dataset_items, key)
+
+    for key in ('duration', 'text', 'audio_path'):
+        result_batch[key] = [item[key] for item in dataset_items]
+
+    result_batch['spectrogram'] = result_batch['spectrogram'].permute(0, 2, 1)
+    result_batch['text_encoded_length'] = torch.tensor(list(item['text_encoded'].size(-1) for item in dataset_items), dtype=torch.int32)
+    result_batch['spectrogram_length'] = torch.tensor(list(item['spectrogram'].size(-1) for item in dataset_items), dtype=torch.int32)
+    return result_batch
--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,20 @@
 # Don't forget to support cases when target_text == ''
+import editdistance
+
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if target_text == '':
+        if predicted_text == '':
+            return 0.
+        return float('Inf')
+    dist = editdistance.distance(target_text.split(), predicted_text.split())
+    return dist / len(target_text.split())
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if target_text == '':
+        if predicted_text == '':
+            return 0.
+        return float('Inf')
+    dist = editdistance.distance(target_text, predicted_text)
+    return dist / len(target_text)
