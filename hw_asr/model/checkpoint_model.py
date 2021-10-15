53e69d79fa18aa6ef7400d5048411ed8954ff488
ArseniyBolotin <arseniy.bolotin@gmail.com>
2021-10-15 10:43:25 +0300

    checkpoint config

---
+++ hw_asr/model/checkpoint_model.py
@@ -0,0 +1,43 @@
+from torch import nn
+import torch.nn.functional as F
+
+from hw_asr.base import BaseModel
+
+
+class M5(BaseModel):
+    def __init__(self, n_feats, n_class, n_input=1, stride=16, n_channel=32, *args, **kwargs):
+        super().__init__(n_feats, n_class, *args, **kwargs)
+        self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
+        self.bn1 = nn.BatchNorm1d(n_channel)
+        self.pool1 = nn.MaxPool1d(4)
+        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(n_channel)
+        self.pool2 = nn.MaxPool1d(4)
+        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(2 * n_channel)
+        self.pool3 = nn.MaxPool1d(4)
+        self.conv4 = nn.Conv1d(2 * n_channel, 2 * n_channel, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(2 * n_channel)
+        self.pool4 = nn.MaxPool1d(4)
+        self.fc1 = nn.Linear(2 * n_channel, n_class)
+
+    def forward(self, spectrogram, *args, **kwargs):
+        x = self.conv1(spectrogram)
+        x = F.relu(self.bn1(x))
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(self.bn2(x))
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = F.relu(self.bn3(x))
+        x = self.pool3(x)
+        x = self.conv4(x)
+        x = F.relu(self.bn4(x))
+        x = self.pool4(x)
+        x = F.avg_pool1d(x, x.shape[-1])
+        x = x.permute(0, 2, 1)
+        x = self.fc1(x)
+        return {"logits": x}
+
+    def transform_input_lengths(self, input_lengths):
+        return input_lengths  # we don't reduce time dimension here
